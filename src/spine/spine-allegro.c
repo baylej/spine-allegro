8264c17b3171f466a8172f4c724351bbe2c9af8f
Thomas Medioni <medioni.thomas@gmail.com>
2013-08-24 15:18:36 +0200

    add fct to init a skeletondrawable

--- src/spine/spine-allegro.c
+++ src/spine/spine-allegro.c
@@ -10,8 +10,8 @@ static ALLEGRO_BITMAP* _spalleg_load_bitmap (const char* path) {
 	al_path = al_create_path(path);
 	if (!al_path) return NULL;
 	
-	image = al_load_bitmap(al_path_cstr(path, '/'));
-	al_destroy_path(path);
+	image = al_load_bitmap(al_path_cstr(al_path, '/'));
+	al_destroy_path(al_path);
 	return image;
 }
 
@@ -26,4 +26,21 @@ void _AtlasPage_createTexture (AtlasPage* self, const char* path) {
 
 void _AtlasPage_disposeTexture (AtlasPage* self) {
 	al_destroy_bitmap((ALLEGRO_BITMAP*)self->rendererObject);
+}
+
+char* _Util_readFile (const char* path, int* length) {
+	return _readFile(path, length);
+}
+
+
+SkeletonDrawable* spalleg_load_skeleton(SkeletonData* skeleton, AnimationStateData* stateData)
+{
+	SkeletonDrawable* sd = malloc(sizeof(SkeletonDrawable));
+	sd->timeScale = 1.;
+	Bone_setYDown(true);
+
+	sd->skeleton = Skeleton_create(skeleton);
+	sd->state = AnimationState_create(stateData);
+
+	return sd;
 }
--- src/spine/spine-allegro.h
+++ src/spine/spine-allegro.h
@@ -9,6 +9,17 @@
 #ifndef _SPINE_ALLEG_H
 #define _SPINE_ALLEG_H
 
+#include <spine/spine.h>
 
+struct SkeletonDrawable
+{
+	Skeleton* skeleton;
+	AnimationState* state;
+	float timeScale;
+	
+};
+typedef struct SkeletonDrawable SkeletonDrawable;
+SkeletonDrawable* spalleg_load_skeleton (SkeletonData* skeleton, AnimationStateData* stateData);
+void spalleg_dispose_skeleton( SkeletonDrawable* skeleton);
 
 #endif /* _SPINE_ALLEG_H */
